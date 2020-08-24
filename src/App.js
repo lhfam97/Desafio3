c8093e2d99d90166e5311f937d01d0ad3c5b9ee7
Luís Henrique Machado <lhfam97@gmail.com>
2020-08-24 19:14:05 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    
+    const response = await api.post('repositories', {
+      title: `Novo repositório ${Date.now()}`,
+      url: 'https://github.com/Diziano/desafio-conceitos-reactjs',
+      techs: ["JavaScript", "ReactJS"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+  }
+
+  async function handleRemoveRepository(id) {
+    const response = await api.delete(`repositories/${id}`);
+
+    const filteredRepositories = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(filteredRepositories);
+  }
+
+  return (
+    <div>
+
+      <ul data-testid="repository-list">
+        { repositories.map(repository => (
+
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+
+        )) }
+        
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
